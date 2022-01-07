029d2510cd7b16e85ff38becb7295ff5f345125c
Pig Fang <g-plane@hotmail.com>
2022-01-07 15:59:20 +0800

    fix stupid logic mistake

--- _shared.ts
+++ _shared.ts
@@ -1,6 +1,6 @@
 export function hasID3(bytes: Uint8Array): boolean {
-  return bytes[0] !== /* "I" */ 0x49 || bytes[1] !== /* "D" */ 0x44 ||
-    bytes[2] !== /* "3" */ 0x33;
+  return bytes[0] === /* "I" */ 0x49 && bytes[1] === /* "D" */ 0x44 &&
+    bytes[2] === /* "3" */ 0x33;
 }
 
 export function readSize(bytes: Uint8Array): number {
